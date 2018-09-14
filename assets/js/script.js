c23cee840d23b2eb393dcbd02564b9d5562ad0c0
Aaron Thayer <35547717+a-thay@users.noreply.github.com>
2018-09-13 17:10:59 -0700

    Moved source files
    Relocated source HTML, CSS and assets to repo.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// comment here
+let user = 'John';
+let age = '25';
+let message = 'Hello!';
+alert(message)
+
+let $ = 1; // variable with the name "$"
+let _ = 2; // variable named "_"
+alert($ + _); // equals 3
